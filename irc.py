9d621ffc1baff975a54b1aeb9487fd6633814dd2
delucks <jamie.delucks@gmail.com>
2015-09-22 13:12:43 -0400

    Added proper message queueing until \r\n, part support, start of message processing bit

--- irc.py
+++ irc.py
@@ -5,14 +5,11 @@ import re
 import time
 
 logging.basicConfig(format='%(asctime)s %(message)s', stream=sys.stderr, level=logging.DEBUG)
+VERSION='0.01'
 
 ''' sets up the bots connection on a socket level.
-can expose methods like "send_msg" or "priv_msg" as a high-level interface,
-with methods for closing, restarting, registering, and all the utility parts of the
-irc protocol
-
-can recieve PING messages at any time, must respond with PONG within {timeout} secs or we'll get kicked
-maybe we should make this class magic- you just instantiate it and then call it in a looop/generator expression to deliver all of your delicious utf-8 goodness
+exposes methods for closing, restarting, registering, and all the 
+utility parts of the irc protocol
 '''
 class IRCConnection:
     def __init__(self, addr, port, timeout=10.0):
@@ -27,35 +24,35 @@ class IRCConnection:
 
     # close down everything that needs to be closed
     def __exit__(self, type, value, traceback):
+        self.send('QUIT :pbjbt/' + VERSION)
         self.conn.close()
 
+    # basic helper method to tack on a \r\n and log it
     def send(self, message):
         self.conn.send(message + '\r\n')
         logging.debug('SEND ' + message)
 
+    def part(self, channel):
+        self.send('PART {0}'.format(channel))
+
     def join(self, channel):
         self.send('JOIN {0}'.format(channel))
 
-    '''
-    there must be a better way to do this
-        maybe something like we recv infinitely, then fire off callback methods if a regex matches?
-        pass in a dict of regex -> callbacks?
-    alternately, we could use polymorphism
-        like an IRCconnection is a generic handler class that defines a method that recv's forever, passing off each line to a predefined
-        method that you can override in the subclasses
-    alternately, we could make a container class that holds an IRCConnection as a property
-        then, that class could consume events from the recv_forever() kinda method and decide on its own callbacks
-    '''
     def recv_forever(self, recv_bufsz=1024):
         read = ''
         while True:
             try:
-                # TODO: read buffer & scaling recv buffer size & split/yield on newlines
                 read = self.conn.recv(recv_bufsz)
                 if read.strip() == '':
                     continue
-                logging.info('RECV ' + read)
-                yield read
+                while '\n' in read:
+                    spl = read.split('\r\n', 1)
+                    if len(spl) > 1:
+                        logging.info('RECV ' + spl[0])
+                        yield spl[0]
+                        read = spl[1]
+                    else:
+                        break # recv 1024 more bytes, looking for a \n
             except socket.timeout:
                 print 'Got a socket.timeout, carrying on'
                 continue
@@ -69,8 +66,6 @@ class IRCConnection:
     def register(self, user, nick, realname=None):
         if realname == None:
             realname = user
-        # TODO some servers, wait for PING :randomstr and respond with PONG :randomstr
-        # TODO: this doesn't work on servers that aren't running the kind of IRC server that irc.lug does
         self.send_rg_msg(user, nick, realname)
 
 ''' handles IRC interactions from a high level (user visible)
@@ -81,31 +76,51 @@ class IRCBot:
         self.name = name
         self.conn = connection
         self.realname = realname
+        self.channels = []
         self.init_channels = init_channels
 
     def join(self, channels):
         for channel in channels:
+            self.channels.append(channel)
             self.conn.join(channel)
 
-    def split_msg(self, message):
-        msg_d = {}
-        for item in message.split('\n'):
-            print item
-        m = re.match('^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$', message)
-        print m.groups()
+    def part(self, channels):
+        for channel in channels:
+            self.channels.remove(channel)
+            self.conn.part(channel)
 
-    # TODO: handle the split message dict
+    # Parse the message into a convenient dictionary (TODO: direct log this as JSON?)
+    def split_msg(self, message):
+        m = re.match('^(?:[:](\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$', message)
+        g = m.groups()
+        return {
+                'hostmask': g[0],
+                'type': g[1],
+                'dest': g[2],
+                'message': g[3]
+        }
+
+    # Make sure this method DOES NOT BLOCK! You can hold up the whole bot because it's single threaded!
     def handle(self, msg_object):
-        pass
+        if msg_object['type'] == 'PING':
+            self.conn.send('PONG :' + msg_object['message'])
+        if msg_object['dest'] == self.nick:
+            # do something if someone messages you
+            pass
+        elif msg_object['dest'] in self.channels:
+            # do something if there's a message that comes up in a channel you're in
+            pass
+        else:
+            # do something! :D
+            pass
 
     def run(self):
         self.conn.register(self.nick, self.name, self.realname)
         if self.init_channels is not None:
             self.join(self.init_channels)
-        # TODO: this doesn't work yet because messages are delimited by 1024 bytes instead of \n
         for recv_irc_msg in self.conn.recv_forever():
-            self.split_msg(recv_irc_msg)
-            #self.handle(recv_irc_msg) # this is why handle shouldn't block
+            split = self.split_msg(recv_irc_msg)
+            self.handle(split) # this is why handle shouldn't block
 
 #net = 'irc.lug.udel.edu'
 net = '127.0.0.1'
