41c162601cefaedd13bca95181481b35bdecb9a2
delucks <jamie.delucks@gmail.com>
2015-11-30 01:47:05 -0500

    Completed class decoration, cleaned it all up.

--- irc.py
+++ irc.py
@@ -4,10 +4,13 @@ import sys
 import re
 import time
 import argparse
+import inspect
+from collections import OrderedDict
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 channelize = lambda x: ['#'+c if not c.startswith('#') else c for c in x]
-VERSION='0.02'
+VERSION='0.03'
+
 
 ''' sets up the bots connection on a socket level.
 exposes methods for closing, restarting, registering, and all the 
@@ -64,17 +67,85 @@ class IRCConnection:
         self.send('NICK {0}'.format(nick))
         self.send('USER {0} {0} {2} :{1}'.format(user, realname, hostname))
 
+''' Decorator to match a regular expression against messages
+then modify the arguments to the decorated function to pull out different properties
+of the message.
+This essentially makes each subsequent method a template for this macro-
+    the last argument will always be filled in by the macro
+    the regex and handling stragegy get read by the macro and stay static
+    when called the method has access to non-static information like socket connection
+The checks in the beginning which return False are for early termination of
+the method before we hit any expensive code paths (regex'ing the message)
+'''
+def wrap_command(func, cmd_regex, arg_handling):
+    def wrapped(*args):
+        msg_object = args[1]
+        #TODO: get rid of the "return None" here and instead have it return something more sane
+        if msg_object['type'] == 'PING':
+            return False
+        if not msg_object['type'] == 'PRIVMSG': # commands only apply to private/public messages
+            return False
+        message = msg_object['message']
+        match = re.match(cmd_regex, message)
+        if not match:
+            logging.debug('{0} failed to match {1}'.format(func.__name__, message))
+            return False
+        if arg_handling == 'group':
+            newargs = (args[0], args[1], match.groups())
+        elif arg_handling == 'first':
+            newargs = (args[0], args[1], message.split()[1])
+        elif arg_handling == 'pass':
+            newargs = (args[0], args[1], message.split()[1:])
+        else: # none
+            newargs = args
+        logging.debug('bot_command[decorator context] calling {0} with {1}'.format(func.__name__, newargs))
+        func(*newargs)
+    return wrapped
+
+''' marks a method as a command within a subclass of IRCBot.
+It can then interact with incoming irc messages after the
+class is activated with @activebot()
+'''
+def bot_command(cmd_regex, arg_handling):
+    def command_decorator(func):
+        func._cmd = True
+        func._cmd_regex = cmd_regex
+        func._arg_handling = arg_handling
+        return func
+    return command_decorator
+
+''' marks a subclass of IRCBot as the active bot to use.
+Activates all of the @bot_command(...) commands by decorating
+them with wrap_command(...)
+'''
+def activebot():
+    def wrap_bot_class(bot_class):
+        for method_name, method in inspect.getmembers(bot_class, inspect.ismethod):
+            if '_cmd' in dir(method): # this is SO HACKY
+                # this is a command method, and needs to be decorated
+                wrapped = wrap_command(method, method._cmd_regex, method._arg_handling)
+                bot_class.commands[method_name] = wrapped
+                # we're manually decorating because fuck you iteration
+                setattr(bot_class, method_name, wrapped)
+        return bot_class
+    return wrap_bot_class
+
+
 ''' handles IRC interactions from a higher level (common utility functions)
-this class should be subclassed in order to build a working bot that can do more
-than join channels
-PLANNING:
-    Maybe we could decorate this with something that decorates all methods starting with handle_ with our regex func
-    However it'd have to happen for any subclass too... can we have an @IRCBot decorator that just makes the class
-    a magic subclass of IRCBot... having access to all the utility functions, and autodecorating these things?
-    Then we could call that base class/decorator BaseIRCBot and inherit from it or whatever
+Subclass this in order to build a working bot.
 '''
 class IRCBot(object):
-    def __init__(self, connection, nick, name, hostname, realname, init_channels):
+    # this is in the class scope so it can be modified by activebot
+    commands = OrderedDict()
+    # also- let's replace this with another class that can have more metadata about each command TODO
+
+    def __init__(self):
+        raise NotImplementedError('You must subclass IRCBot, then call create()')
+
+    ''' initializes a subclass of IRCBot.
+    Done this way so you cannot directly initialize this class
+    '''
+    def create(self, connection, nick, name, hostname, realname, init_channels):
         self.nick = nick
         self.name = name
         self.conn = connection
@@ -148,11 +219,6 @@ class IRCBot(object):
             info['type'] = msg_type
             yield info
 
-    ''' this method should be defined by the bot implementer
-    '''
-    def handle(self, msg_object):
-        raise NotImplementedError()
-
     ''' set up the bot, join initial channels, start the loop
     '''
     def run(self):
@@ -164,90 +230,68 @@ class IRCBot(object):
             logging.debug('IRCBotBase: Calling handle() on {0}'.format(split))
             self.handle(split) # this is why handle shouldn't block
 
-#class pbjbt(IRCBotBase):
-#    def __init__(self, connection, nick, name, hostname, realname=None, init_channels=None):
-#        self.votes = {} # TODO some kind of session persistence
-#        super(IRCBot, self).__init__(
-#                connection,
-#                nick = 'pbjbt',
-#                name = 'pbjbt',
-#                hostname,
-#                realname,
-#                init_channels
-#        )
-
-    ''' takes a split message object and fires off a bunch of methods to handle it
+    ''' takes a split message object and calls every command method
+    The command methods will early-terminate with False return status if not applicable
+    Make sure they do not block, this whole thing is single-threaded
     '''
     def handle(self, msg_object):
         # This is an example of how you would parse a field other than 'message' in this method b4 handlers
         if msg_object['type'] == 'PING':
             self.conn.send('PONG :' + msg_object['message'])
-        self.handle_version(msg_object)
-        self.handle_join(msg_object)
-        self.handle_ping(msg_object)
-        self.handle_plusplus(msg_object)
+        else:
+            for method_name, method in self.commands.iteritems():
+                if method(self, msg_object): # the call
+                    logging.info('Called command method {0}'.format(method_name))
+                else:
+                    logging.debug('{0} failed to match {1}'.format(method_name, msg_object))
 
-    ''' Decorator to match a regular expression against messages
-    then modify the arguments to the decorated function to pull out different properties
-    of the message.
-    This essentially makes each subsequent method a template for this macro-
-        the last argument will always be filled in by the macro
-        the regex and handling stragegy get read by the macro and stay static
-        when called the method has access to non-static information like socket connection
-    not bound to 'self' because decorator is compiled before objects are instantiated
-    the ideal situation here is to calculate the theoretical string intersection of all the regexes
+    ''' Commands in common between every IRCBot subclass:
     '''
-    def add_command(cmd_regex, arg_handling):
-        print('Inside add_command')
-        def command_decorator(func):
-            print('Inside command_decorator context but not the wrapped func yet')
-            #self = args[0] # this is so hacky
-            #if cmd_regex in self.commands:
-            #    logging.fatal('Command regular expression registered twice! Offending regex and method: {0} {1}'.format(
-            #        cmd_regex, 
-            #    self.commands[cmd_regex] =
-            def wrapped(*args):
-                msg_object = args[1]
-                '''
-                technically you could get at 'self' with args[0]
-                if that's the case you can call a self.register_command(... so that the regex is in a dict
-                but if you want to do that, you'd have to use a global and put the actual code to add the command
-                up inside the command_decorator context up there ^^
-                you can only get at self in runtime. so if we want to define commands at runtime they can't be in the decorator
-                this is why logging does not work inside the decorator
-                TODO: get rid of the "return None" here and instead have it return something more sane
-                '''
-                if not msg_object['type'] == 'PRIVMSG': # commands only apply to private/public messages
-                    return None
-                message = msg_object['message']
-                match = re.match(cmd_regex, message)
-                if not match:
-                    logging.debug('{0} failed to match {1}'.format(func.__name__, message))
-                    return None
-                if arg_handling == 'group':
-                    newargs = (args[0], args[1], match.groups())
-                elif arg_handling == 'first':
-                    newargs = (args[0], args[1], message.split()[1])
-                elif arg_handling == 'pass':
-                    newargs = (args[0], args[1], message.split()[1:])
-                else: # none
-                    newargs = args
-                logging.debug('add_command[decorator context] calling {0} with {1}'.format(func.__name__, newargs))
-                func(*newargs)
-            return wrapped
-        return command_decorator
+    @bot_command('^\.join', 'pass')
+    def join_multi(self, msg_object, channels):
+        if len(channels) < 1:
+            self.conn.message(msg_object['dest'], 'Usage: .join #channelname')
+            return False # this now early terminates
+        logging.debug('We have channels! joining them.')
+        chans = channelize(channels)
+        self.join(chans)
 
-    # something like this, then the class' handler gets each of the methods
-    #def add_command(cmd_regex, arg_handling):
-    #    def command_decorator(func):
-    #        func._cmd = True
-    #        func._cmd_regex = cmd_regex
-    #        func._arg_handling = arg_handling
-    #        return func
-    #    return command_decorator
+    @bot_command('^\.version', 'none')
+    def version(self, msg_object):
+        nick = msg_object['nick']
+        self.conn.message(msg_object['dest'], '{2}: {0} version {1}'.format(self.nick, VERSION, nick))
 
-    @add_command('^([a-zA-Z0-9]+)(\+\+|--|\*\*)', 'group')
-    def handle_plusplus(self, msg_object, match_groups):
+    @bot_command('^\.ping', 'none')
+    def ping(self, msg_object):
+        nick = msg_object['nick']
+        self.conn.message(msg_object['dest'], '{0}: pong'.format(nick))
+
+    @bot_command('^\.commands', 'none')
+    def list_commands(self, msg_object):
+        # TODO: use docstring of each method for its help text
+        nick = msg_object['nick']
+        self.conn.message(msg_object['dest'], '{0}: {1}'.format(nick, ','.join(self.commands.keys())))
+
+
+''' pbjbt is a simple bot. it uses 'pbjbt' for
+all of its names, and only has the ability to modify
+the origin hostname
+'''
+@activebot()
+class pbjbt(IRCBot):
+    def __init__(self, connection, hostname, init_channels):
+        self.votes = {} # TODO some kind of session persistence
+        super(pbjbt, self).create(
+                connection,
+                self.__class__.__name__, # nick
+                self.__class__.__name__, # name
+                hostname,
+                self.__class__.__name__, # realname
+                init_channels
+        )
+
+    @bot_command('^([a-zA-Z0-9]+)(\+\+|--|\*\*)', 'group')
+    def increment(self, msg_object, match_groups):
         return_votes = lambda x: 'voted {0} (+{1} / -{2})'.format(x[0]-x[1], x[0], x[1])
         some_str = match_groups[0]
         oper = match_groups
@@ -265,40 +309,6 @@ class IRCBot(object):
         else:
             self.conn.message(msg_object['dest'], '{0}: {1}'.format(some_str, return_votes(self.votes[some_str])))
 
-    @add_command('^\.join', 'pass')
-    def handle_join(self, msg_object, channels):
-        if msg_object['type'] == 'PRIVMSG':
-            if len(channels) < 1:
-                self.conn.message(msg_object['dest'], 'Usage: .join #channelname')
-                return None
-            logging.debug('We have channels! joining them.')
-            chans = channelize(channels)
-            self.join(chans)
-        else:
-            logging.debug('NOT A PRIVMSG DAMN')
-
-    @add_command('^\.version', 'none')
-    def handle_version(self, msg_object):
-        if msg_object['type'] == 'PRIVMSG':
-            nick = msg_object['nick']
-            self.conn.message(msg_object['dest'], '{2}: {0} version {1}'.format(self.nick, VERSION, nick))
-
-    @add_command('^\.ping', 'none')
-    def handle_ping(self, msg_object):
-        if msg_object['type'] == 'PRIVMSG':
-            nick = msg_object['nick']
-            self.conn.message(msg_object['dest'], '{0}: pong'.format(nick))
-
-'''
-so we need some kind of a method that can decorate a subclass of IRCBot
-this decorator must target all methods decorated with the @command dummy deco
-command() will be defined in IRCBot with pass
-1. take original class, make a copy
-2. iter through class' methods
-3. check if @command
-4. if so, populate the self.commands dict with regex, group handling, and method signature
-'''
-
 def interactive():
     p = argparse.ArgumentParser(description='pbjbt')
     p.add_argument('-n', '--network', help='FQDN of IRC network to connect to', default='127.0.0.1')
@@ -315,11 +325,8 @@ def interactive():
     if args.channels:
         args.channels = channelize(args.channels)
     with IRCConnection(args.network, args.port) as c:
-        bot = IRCBot(
+        bot = pbjbt(
                 connection=c,
-                nick=args.nick,
-                name=args.name,
-                realname=args.realname,
                 hostname=args.hostname,
                 init_channels=args.channels
         )
