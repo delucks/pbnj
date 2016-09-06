169214c5815ed1adb6c19d5e98e5d46cb869c276
delucks <jamie.delucks@gmail.com>
2016-09-06 06:58:12 +0000

    Fix path of precommit

--- etc/install-pep8-precommit.sh
+++ etc/install-pep8-precommit.sh
@@ -1,10 +1,6 @@
 cat << DOG > .git/hooks/pre-commit
-#!/usr/bin/env bash
-function cmd_exists() {
-  eval "which \$1" >/dev/null 2>&1
-}
-if cmd_exists pep8
-then
-  pep8 irc.py
+#!/usr/bin/env sh
+if hash flake8; then
+  flake8 pbnj/
 fi
 DOG
