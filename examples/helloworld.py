d0e2cd07dd0953c2a665b272d43bf7e6585b61aa
delucks <jamie.delucks@gmail.com>
2016-09-08 07:02:11 +0000

    Separated out the lambda demonstration from "helloworld" example

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -10,12 +10,6 @@ def helloworld(message):
     '''says hello world'''
     return '{0}: Hello world!'.format(message.nick)
 
-match_shrugs = lambda m: m.type == 'ACTION' and m.message.startswith('shrug')
-@bot.command(match_shrugs)  # using a callable, takes in a single pbnj.models.Message object
-def shrug(message):
-    '''shrugs'''
-    return '{}: ¯\_(ツ)_/¯'.format(message.nick)
-
 if __name__ == '__main__':
     bot._parse_args(docstring=__doc__, override=True)
     bot.run()
---
+++ examples/lambda.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''shrugbot here shows us how to use a lambda as a command filter, as well as
+how to work with ACTION (/me) messages'''
+from pbnj.bot import Bot
+
+bot = Bot('meh')
+
+# the callable gets a single pbnj.models.Message object
+match_shrugs = lambda m: m.type == 'ACTION' and m.message.startswith('shrug')
+@bot.command(match_shrugs)
+def shrug(message):
+    '''shrugs'''
+    return '{}: ¯\_(ツ)_/¯'.format(message.nick)
+
+if __name__ == '__main__':
+    bot._parse_args(docstring=__doc__)
+    bot.run()
